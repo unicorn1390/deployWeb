21aaf30fa650e3c109adbeddd9b7e9cf32f4fd02
tangfan <tangfan@nibirutech.com>
2016-11-25 10:22:15 +0800

    a

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'index/index'
+  root 'index#index'
+
+  resources :articles
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
